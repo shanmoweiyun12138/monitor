780bec4fecd87ba22da507badedff3f8f28b164b
wangjianguan <wang-jg@139.com>
2017-09-24 11:36:48 +0800

    修改成公网地址

--- js/public.js
+++ js/public.js
@@ -2,5 +2,7 @@
  * Created by wangjg on 2017/9/23.
  */
 
-
-var serverUrl = "http://localhost:8080/";
+// 本机地址
+//var serverUrl = "http://localhost:8080/";
+//公网地址
+var serverUrl = "http://wangjianguan12138.tunnel.qydev.com";
